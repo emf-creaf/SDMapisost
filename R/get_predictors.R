1870bd72139accdc8881f7b37e2ee00ccb0da7cb
Roberto Molowny-Horas <roberto@creaf.uab.es>
2026-06-05 15:39:14 +0200

    Changed in "get_predictors" and "get_presence". Additions to "seleccion-variables". Now there are 3 different methods available.

--- R/get_predictors.R
+++ R/get_predictors.R
@@ -5,8 +5,9 @@
 #' that are used in the analysis at all presence locations for the input
 #' species.
 #'
-#' @param carpeta \code{character} string
-#' @param especie
+#' @param folder \code{character} string with the root name of the folder with the data.
+#' @param species \code{character} string with the full name of the species.
+#' @param verbose \code{logical}, if TRUE progress information is produced.
 #'
 #' @returns
 #' A \code{terra} SpatVector object.
@@ -14,25 +15,28 @@
 #' @export
 #'
 #' @examples
-#' carpeta <- "C:/imidra/"
-#' nombre_especie <- "Cistus ladanifer"
-#' df <- get_predictors(carpeta, nombre_especie)
-get_predictors <- function(carpeta, especie) {
+#' folder <- "C:/imidra/"
+#' species <- "Cistus ladanifer"
+#' x <- get_predictors(folder, species)
+get_predictors <- function(folder, species, verbose = TRUE) {
 
-  # Datos de presencia de la especie.
-  p <- datos_especie(file.path(carpeta, "ifn"), nombre_especie) |>
+  # Presence data for species.
+  p <- get_presence(folder, species, verbose = verbose) |>
     sf::st_geometry() |>
     terra::vect()
 
 
-  # Elevación y orientación.
-  mdt <- terra::rast(file.path(carpeta, "mdt", "mdt_madrid.tif"))
+  # Elevation and orientation.
+  mdt <- terra::rast(file.path(folder, "mdt", "mdt_madrid.tif"))
+
   slope <- terra::terrain(mdt, v = "slope", neighbors = 8, unit = "radians")
   aspect <- terra::terrain(mdt, v = "aspect", neighbors = 8, unit = "radians")
   slope <- tan(slope)*100
   northness <- cos(aspect)
   eastness <- sin(aspect)
 
+
+  if (verbose) cli::cli_alert_info("Extracting terrain characteristics at presence locations")
   p <- terra::project(p, terra::crs(mdt))
   p <- cbind(p,
              terra::extract(mdt, p, ID = FALSE, method = "bilinear"),
@@ -42,9 +46,10 @@ get_predictors <- function(carpeta, especie) {
   names(p) <- c("mdt", "pendiente", "norte", "este")
 
 
-  # Variables bioclimáticas.
+  # Bioclimatic variables.
+  if (verbose) cli::cli_alert_info("Extracting bioclimatic data at presence locations")
   for (i in 1:19) {
-    biov <- terra::rast(file.path(carpeta, "bioclim/historico/wc2.1_10m_bio",
+    biov <- terra::rast(file.path(folder, "bioclim/historico/wc2.1_10m_bio",
                                   paste0("wc2.1_10m_bio_", i, ".tif")))
     if (i == 1) p <- terra::project(p, terra::crs(biov))
     p <- cbind(p, terra::extract(biov, p, ID = FALSE, method = "bilinear"))
@@ -53,12 +58,16 @@ get_predictors <- function(carpeta, especie) {
 
 
   # CORINE.
-  corine <- sf::st_read(file.path(carpeta, "/corine/corine_2018", "Madrid_CLC2018.gpkg"),
+  if (verbose) cli::cli_alert_info("Extracting CORINE data at presence locations")
+  corine <- sf::st_read(file.path(folder, "/corine/corine_2018", "Madrid_CLC2018.gpkg"),
                         quiet = TRUE) |>
     terra::vect()
   p <- terra::project(p, terra::crs(corine))
   p <- cbind(p, terra::extract(corine, p)[, "CODE_18"])
   names(p)[ncol(p)] <- "corine"
+  clc_classes <- c("artificial", "agricultural", "natural", "wetlands", "water bodies")
+  p$corine <- clc_classes[as.numeric(substr(p$corine, 1, 1))]
+
 
   return(p)
 
--- R/get_presence.R
+++ R/get_presence.R
@@ -1,36 +1,37 @@
 #' Title
 #'
-#' @param carpeta Cadena de caracteres indicando la carpeta en la que encontrar los archivos
-#' de datos ".rds" de los IFN.
-#' @param nombre_especie Cadena de caracteres con el nombre en latín de la especie.
-#' @param ifn Valor numérico indicando qué IFN hay que leer. Debe ser 2, 3 o 4.
+#' @param folder \code{character} string indicating the root folder to retrieve the data from.
+#' @param species \code{character} string with the full name of the species.
+#' @param ifn \code{numeric} value indicating which IFN to read. It can be equal to 2, 3 or 4.
 #'
 #' @returns
-#' Un objecto "sf" con el contenido de los datos de presencia de la especie.
+#' An "sf" object containing presence data for the species.
 #' @export
 #'
 #' @examples
-#' carpeta <- "C:/imidra/ifn"
-#' nombre_especie <- "Cistus ladanifer"
-#' df <- datos_especie(carpeta, nombre_especie)
-datos_especie <- function(carpeta, nombre_especie, ifn = 2) {
+#' folder <- "C:/imidra"
+#' species <- "Cistus ladanifer"
+#' x <- get_presence(folder, species)
+#'
+get_presence <- function(folder, species, ifn = 2, verbose = TRUE) {
 
   # Checks.
-  nombre_especie <- trimws(nombre_especie)
-  if (!is.numeric(ifn)) cli::cli_abort("Parámetro de entrada 'ifn' debe ser numérico")
-  if (length(ifn) != 1) cli::cli_abort("Parámetro de entrada 'ifn' debe tener longitud = 1")
-  if (!(ifn %in% c(2, 3, 4))) cli::cli_abort("Parámetro de entrada 'ifn' debe ser igual a 2, 3 o 4")
+  nombre_especie <- trimws(species)
+  if (!is.numeric(ifn)) cli::cli_abort("Input parameter 'ifn' must be numeric")
+  if (length(ifn) != 1) cli::cli_abort("Input parameter 'ifn' must have length = 1")
+  if (!(ifn %in% c(2, 3, 4))) cli::cli_abort("Input parameter 'ifn' must be equal to 2, 3 or 4")
 
 
-  # Leemos archivo para Madrid (código 28).
+  # Read file for Madrid (code 28).
   df <- switch(as.character(ifn),
-               "2" = readRDS(file.path(carpeta, "ifn2_28.rds")),
-               "3" = readRDS(file.path(carpeta, "ifn3_28.rds")),
-               "4" = readRDS(file.path(carpeta, "ifn4_28.rds"))
+               "2" = readRDS(file.path(folder, "ifn", "ifn2_28.rds")),
+               "3" = readRDS(file.path(folder, "ifn", "ifn3_28.rds")),
+               "4" = readRDS(file.path(folder, "ifn", "ifn4_28.rds"))
   )
 
 
-  # Obtenemos datos de presencia para la especie.
+  # Retrieve presence data for species.
+  if (verbose) cli::cli_alert_info(paste0("Retreaving presence data for species ", species))
   x <- df[, c("id_unique_code", "plot", "coordx", "coordy")]
   x$y <- sapply(df$understory, function(z) {
     zz <- z$shrub[[1]]
@@ -42,7 +43,7 @@ datos_especie <- function(carpeta, nombre_especie, ifn = 2) {
   })
 
 
-  # Convertir a un objeto "sf".
+  # Convert to "sf".
   x <- sf::st_as_sf(x,
                     coords = c("coordx", "coordy"),
                     crs = sf::st_crs(paste0("EPSG:", df$crs[1])))
